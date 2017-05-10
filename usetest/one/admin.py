d9720636daf0c34afcbbbe27ce07ecbb0c02c4cc
wayne <278972894@qq.com>
2017-05-10 16:44:53 +0800

    admin

--- usetest/one/admin.py
+++ usetest/one/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from main.models import IpUser, IpRole
+from main.models import IpPermission
 
-# Register your models here.
+
+class IpUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'password', 'parent')
+    search_fields = ('id', 'company')
+
+class IpPermissionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'permname', 'roleperm')
+    search_fields = ('id', 'company')
+
+admin.site.register(IpUser, IpUserAdmin)
+admin.site.register(IpPermission, IpPermissionAdmin)
